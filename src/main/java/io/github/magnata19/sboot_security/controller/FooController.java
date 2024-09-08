d23beb11dd315f37bd018da3acb604836906073a
magnata19 <dehpacifico@gmail.com>
2024-09-08 18:04:03 -0300

    configuracao inicial do spring-security

--- src/main/java/io/github/magnata19/sboot_security/controller/FooController.java
+++ src/main/java/io/github/magnata19/sboot_security/controller/FooController.java
@@ -1,5 +1,6 @@
 package io.github.magnata19.sboot_security.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
+@Slf4j
 @RestController
 public class FooController {
 
