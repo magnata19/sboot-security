ab5686d3a1e17d8ac0743e2f97542a3cf9580997
magnata19 <dehpacifico@gmail.com>
2024-09-13 22:19:10 -0300

    criar nova interface

---
+++ src/main/java/io/github/magnata19/sboot_security/domain/repository/UsuarioGrupoRepository.java
@@ -0,0 +1,7 @@
+package io.github.magnata19.sboot_security.domain.repository;
+
+import io.github.magnata19.sboot_security.domain.entity.UsuarioGrupo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioGrupoRepository extends JpaRepository<UsuarioGrupo, String> {
+}
